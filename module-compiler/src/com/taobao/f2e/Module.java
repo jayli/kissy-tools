523ae1e8f296bc7cc0744198843bbd48cfe69c70
拔赤 <jay.li@alibaba-inc.com>
2013-03-09 12:00:06 +0800

    module compiler Modified by jayli,增加了文件编码识别，和填充module name 的bugfix

--- module-compiler/src/com/taobao/f2e/Module.java
+++ module-compiler/src/com/taobao/f2e/Module.java
@@ -2,6 +2,12 @@ package com.taobao.f2e;
 
 import com.google.javascript.rhino.Node;
 import com.google.javascript.rhino.Token;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.FileInputStream;
+
+import info.monitorenter.cpdetector.io.*;
+import java.nio.charset.Charset;
 
 import java.io.File;
 
@@ -19,6 +25,8 @@ public class Module {
     /**
      * encoding of module 's code file.
      */
+	// added by jayli
+	// 编码要自动识别，不要手动指定
     private String encoding = "utf-8";
     /**
      * module package 's file path.
@@ -79,7 +87,7 @@ public class Module {
         if (content != null) {
             return content;
         } else {
-            return content = FileUtils.getFileContent(fullpath, encoding);
+            return content = FileUtils.getFileContent(fullpath, getEncode(fullpath));
         }
     }
 
@@ -90,16 +98,52 @@ public class Module {
         Module module = this;
         Node moduleNameNode = module.getModuleNameNode();
         if (moduleNameNode.getType() != Token.STRING) {
+			// 补全Modulename
             moduleNameNode.addChildAfter(Node.newString(module.getName()),
                     moduleNameNode.getParent().getChildBefore(moduleNameNode));
             module.setCode(AstUtils.toSource(module.getAstRoot()));
             if (saveToFile) {
-                FileUtils.outputContent(code, fullpath, encoding);
+                //FileUtils.outputContent(code, fullpath, encoding);
+                FileUtils.outputContent(module.getContent().replace("KISSY.add(","KISSY.add(\""+module.getName()+"\","), fullpath, getEncode(fullpath));
             }
         } else {
+			// 写入源文件
             module.setCode(module.getContent());
         }
     }
+
+	// added by jayli
+	public String getEncode(String path){  
+		/*-------------------------------------------------------------------------
+		  使用需要用到三个第三方JAR包：antlr.jar、chardet.jar和cpdetector.jar
+		  cpDetector是基于统计学原理的，不保证完全正确。
+		  --------------------------------------------------------------------------*/
+		CodepageDetectorProxy detector = CodepageDetectorProxy.getInstance();
+
+		/*-------------------------------------------------------------------------
+		  ParsingDetector可用于检查HTML、XML等文件或字符流的编码,构造方法中的参数用于
+		  指示是否显示探测过程的详细信息，为false不显示。
+		  ---------------------------------------------------------------------------*/
+		detector.add(new ParsingDetector(false));
+		detector.add(JChardetFacade.getInstance());//用到antlr.jar、chardet.jar
+
+		// ASCIIDetector用于ASCII编码测定
+		detector.add(ASCIIDetector.getInstance());
+
+		// UnicodeDetector用于Unicode家族编码的测定
+		detector.add(UnicodeDetector.getInstance());
+		java.nio.charset.Charset charset = null;
+		File f = new File(path);
+		try {
+			charset = detector.detectCodepage(f.toURI().toURL());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		if (charset != null)
+			return charset.name();
+		else
+			return null;
+	}
 
     public String[] getRequires() {
         if (requires != null) {
